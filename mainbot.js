4d77a2e63d3b60be15714de01aabb15003a34564
Andrew Ha <ah10@live.com.au>
2020-03-30 22:02:55 +0800

    Global cooldown using coolingdown variable
    Counter starts at 1 to not invoke the andthen at first call

--- mainbot.js
+++ mainbot.js
@@ -13,7 +13,7 @@ const OBSWebSocket = require('obs-websocket-js');
 const opts = {
   identity: {
     username: 'urgabot',
-    password: 'oauth:kq8b9ib4f9z23zy43vc97msawkpt8r'    //from https://twitchapps.com/tmi/
+    password: 'oauth:pmmx576vhmbxa245chdyk5z8tctuaz'    //from https://twitchapps.com/tmi/
   },
   channels: [
     'urgableh'
@@ -22,8 +22,10 @@ const opts = {
 
 // Scene and source constants
 const sceneMain = 'Screen Capture 2'
+const sceneMain2 = 'AndthenScene'
 const sourceMain = 'Andthen'
 const sourceMainEx = 'NoAndthen'
+const waitPeriod = 20           // Global cooldown (s) when triggering alerts to disable again
 
 // Create a client with our options
 const client = new tmi.client(opts);
@@ -42,6 +44,7 @@ obs.connect()
 });
 
 // Register our event handlers (defined below)
+
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
@@ -49,38 +52,55 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 // Global variables for functions
-var counter1 = 0;
+var counter1 = 1;
+var coolingdown = false;
+
+
+function cooldown(arg) {
+  coolingdown = false;
+}
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } 
-
-  // Changes scene between Screen Capture and Screen Capture 2
-  if (commandName === '!changeScene') {
-    changeScenef(target, context, msg, self, commandName);
-    return;
+  if (coolingdown) {
+    console.log('cooling down...');
+    return; 
   }
-
-  // Deactivates then reactivates the visibility of the Andthen alert in Screen Capture 2
-  if (commandName === '!andthen') {
-    andthenf(target, context, msg, self, commandName);
-    return;
-  }
-
   else {
-      console.log(`* Unknown command ${commandName}`);
-  }
+    if (self) { return; } // Ignore messages from the bot
 
+    // Remove whitespace from chat message
+    const commandName = msg.trim();
+
+    // If the command is known, let's execute it
+    if (commandName === '!dice') {
+      const num = rollDice();
+      client.say(target, `You rolled a ${num}`);
+      console.log(`* Executed ${commandName} command`);
+    } 
+
+    // Changes scene between Screen Capture and Screen Capture 2
+    if (commandName === '!changeScene') {
+      changeScenef(target, context, msg, self, commandName);
+      coolingdown = true;   // sets a cooldown variable to true
+      setTimeout(cooldown, waitPeriod*1000);  // calls the function to re-enable commands
+      return;
+    }
+
+    // Deactivates then reactivates the visibility of the Andthen alert in Screen Capture 2
+    if (commandName === '!andthen') {
+      andthenf(target, context, msg, self, commandName);
+      coolingdown = true;   // sets a cooldown variable to true
+      setTimeout(cooldown, waitPeriod*1000);  // calls the function to re-enable commands
+      return;
+    }
+
+    else {
+        console.log(`* Unknown command ${commandName}`);
+    }
+
+  }
+  
 }
 
 //////// FUNCTION DEFINITIONS BELOW ////////
@@ -117,7 +137,7 @@ function changeScenef(target, context, msg, self, commandName){
     //console.log(data);
     if (data["current-scene"] === sceneMain) {
       obs.send('SetCurrentScene', {
-        'scene-name': 'Screen Capture'
+        'scene-name': sceneMain2
       });
     }
   })
@@ -146,6 +166,7 @@ function andthenf(target, context, msg, self, commandName){
         render: true,           // Enable visibility
         "scene-name": sceneMain
       });
+ //     wait(waitPeriod*1000);
     })
     .catch(err => {
       console.log(err);
@@ -170,6 +191,7 @@ function andthenf(target, context, msg, self, commandName){
         render: true,           // Enable visibility
         "scene-name": sceneMain
       });
+ //     wait(waitPeriod*1000);
     })
     .catch(err => {
       console.log(err);
@@ -177,4 +199,20 @@ function andthenf(target, context, msg, self, commandName){
     counter1++;
     console.log(`* Executed ${commandName} command`);     
   }
+/*
+  obs.send('GetSceneList')
+  .then(data => {
+    //console.log(data);
+    obs.send('SetSceneItemRender', {
+      source: sourceMain,
+      render: false,          // Disable visibility
+      "scene-name": sceneMain
+    });
+    obs.send('SetSceneItemRender', {
+      source: sourceMainEx,
+      render: false,          // Disable visibility
+      "scene-name": sceneMain
+    });
+  })
+*/
 }
