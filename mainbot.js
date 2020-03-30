418a2399d3fd5fad1c40e9ca71e6aec16d50323f
Andrew Ha <ah10@live.com.au>
2020-03-30 16:45:14 +0800

    Separated into functions and called by if-statements (Also renamed functions)
    Readability improved.

--- mainbot.js
+++ mainbot.js
@@ -31,7 +31,6 @@ const obs = new OBSWebSocket();
 obs.connect()
 .then(() => {
     console.log(`Success! We're connected & authenticated.`);
-
     return obs.send('GetSceneList');
 })
 .then(data => {
@@ -63,68 +62,37 @@ function onMessageHandler (target, context, msg, self) {
   } 
 
   // Changes scene between Screen Capture and Screen Capture 2
-  if (commandName === '!andthen') {
-      // Bot types in chat
-      client.say(target,`AND THEN?!`);
-      obs.send('SetCurrentScene', {
-        'scene-name': sceneMain
-      });
-      obs.send('GetSceneList')    // Contains most scene and source info
-      .then(data => {
-        //console.log(data);
-        if (data["current-scene"] === sceneMain) {
-          obs.send('SetCurrentScene', {
-            'scene-name': 'Screen Capture'
-          });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    console.log(`* Executed ${commandName} command`);
+  if (commandName === '!changeScene') {
+    changeScenef(target, context, msg, self, commandName);
+    return;
   }
 
   // Deactivates then reactivates the visibility of the Andthen alert in Screen Capture 2
-  if (commandName === '!andthen2') {
-    client.say(target,`AND THEN 2?!`);
-    obs.send('GetSceneList')
-    .then(data => {
-      //console.log(data);
-      obs.send('SetSceneItemRender', {
-        source: sourceMain,
-        render: false,          // Disable visibility
-        "scene-name": sceneMain
-      });
-      wait(100);  // Necessary to wait between setting attributes
-      // It was found that it would ignore one of the requests if it was too fast.
-      obs.send('SetSceneItemRender', {
-        source: sourceMain,
-        render: true,           // Enable visibility
-        "scene-name": sceneMain
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  console.log(`* Executed ${commandName} command`);
+  if (commandName === '!andthen') {
+    andthenf(target, context, msg, self, commandName);
+    return;
   }
       
   else {
       console.log(`* Unknown command ${commandName}`);
   }
-  
 
 }
+
+//////// FUNCTION DEFINITIONS BELOW ////////
+
 // Function called when the "dice" command is issued
 function rollDice () {
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
 }
+
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   //  client.say(opts.channels[0],'/me is now running.');
   console.log(`* Connected to ${addr}:${port}`);
 }
+
 // Function to hold the program for ms seconds in milliseconds
 function wait(ms){
   var start = new Date().getTime();
@@ -132,4 +100,50 @@ function wait(ms){
   while(end < start + ms) {
     end = new Date().getTime();
  }
+}
+
+function changeScenef(target, context, msg, self, commandName){
+  // Bot types in chat
+  client.say(target,`changeScene`);
+  obs.send('SetCurrentScene', {
+    'scene-name': sceneMain
+  });
+  obs.send('GetSceneList')    // Contains most scene and source info
+  .then(data => {
+    //console.log(data);
+    if (data["current-scene"] === sceneMain) {
+      obs.send('SetCurrentScene', {
+        'scene-name': 'Screen Capture'
+      });
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  console.log(`* Executed ${commandName} command`);
+}
+
+// function for andthen
+function andthenf(target, context, msg, self, commandName){
+  client.say(target,`AND THEN?!`);
+  obs.send('GetSceneList')
+  .then(data => {
+    //console.log(data);
+    obs.send('SetSceneItemRender', {
+      source: sourceMain,
+      render: false,          // Disable visibility
+      "scene-name": sceneMain
+    });
+    wait(100);  // Necessary to wait between setting attributes
+    // It was found that it would ignore one of the requests if it was too fast.
+    obs.send('SetSceneItemRender', {
+      source: sourceMain,
+      render: true,           // Enable visibility
+      "scene-name": sceneMain
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  console.log(`* Executed ${commandName} command`);
 }
